307dc904a94705f3444de0ee4db906310917b085
Luis Angel <santosangel748@gmail.com>
2020-10-12 15:40:05 -0500

    Servicio para obtener videos de YouTube

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'AIzaSyCOA8Ve5kIgF-dKbq9FcniOAMLoqVheGWU';
+  private playlist = 'UUFR2oaNj02WnXkOgLH0iqOA';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) {  }
+
+  getVideos(){
+    const url = `${this.youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this.playlist)
+      .set('key', this.apikey)
+
+    return this.http.get(url, {params});
+  }
 }
